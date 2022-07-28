e3b3fd5dd52798e92047c5bbfe20fdefba61b9d6
Vladimir Sviyazov <sva125125@gmail.com>
2022-07-28 22:59:21 +0300

    [Feature] Rewrote `DiscreteWienerProcess`
    The class now generates several samples, not 1. Class `DiscreteWienerProcessSampling` is no longer needed

--- brownian_motion.py
+++ brownian_motion.py
@@ -1,5 +1,5 @@
-from scipy import stats as sts
-import numpy as np
+from scipy.stats import norm
+from numpy import append, array
 
 
 class DiscreteWienerProcess:
@@ -7,41 +7,53 @@ class DiscreteWienerProcess:
     Class representing a discrete Wiener process.
     """
     def __init__(self,
-                 # n: int = 100,
                  initial_position: float = 0,
                  delta_t: float = 1):
-        # self.n = n
         self.initial_position = initial_position
-        self.steps = None
-        self.path = None
         self.delta_t = delta_t
-        self.n = None
 
-    def generate_path(self,
-                      n: int):
+        self.n = None
+        self.n_samples = None
+
+        self.steps = None
+        self.paths = None
+
+    def _generate_steps(self,
+                        n_steps: int) -> array:
         """
-        Generate a random path of length `n` (including starting position)
-        :param n: int, the length of the path (# of discrete process states). It includes the starting point.
-        Thus, for example, if `n = 100`, 99 states will be generated
+        :param n_steps: `int`, number of steps
+        :return: `numpy.array`, steps at each delta t
+        """
+
+        steps = norm(loc=0, scale=self.delta_t ** .5).rvs(size=n_steps)
+        return steps
+
+    def _generate_path(self,
+                       steps: array) -> array:
+        """
+        Generate a random path given steps (increments at each delta t). Resulting array will contain 1 more element
+        than `steps` array. For instance, if 100 steps are given, the resulting array will have 101 process states
+        :param steps: `numpy.array`, generated steps (at each delta t)
+        :return: `numpy.array`, discrete consequent process states
+        """
+
+        path = append(self.initial_position, steps.cumsum() + self.initial_position).copy()
+        return path
+
+    def generate_paths(self,
+                       n: int,
+                       n_samples: int = 1):
+        """
+        :param n: `int`, number of process states in each sample. Note that it is by 1 larger than # of steps. Thus,
+        for instance, if n = 100, the first state will be according to `initial_position`
+        and 99 random states will be generated
+        :param n_samples: `int`, number of paths
         """
         self.n = n
-        self.steps = sts.norm(loc=0, scale=self.delta_t ** .5).rvs(size=self.n - 1)
-        self.path = np.append(self.initial_position, self.steps.cumsum() + self.initial_position).copy()
-
-
-class DiscreteWienerProcessSampling(DiscreteWienerProcess):
-    def __init__(self,
-                 n: int = 100,
-                 initial_position: float = 0,
-                 n_samples: int = 10,
-                 delta_t: float = 1):
-        super().__init__(self, n=n, initial_position=initial_position, delta_t=delta_t)
         self.n_samples = n_samples
-        self.steps_list = [np.full(shape=(n_samples, n - 1), fill_value=np.nan)]
-        self.paths = np.full(shape=(n_samples, n), fill_value=np.nan)
 
-    def generate_paths(self):
-        for i in range(self.n_samples):
-            self.generate_path()
-            self.steps_list[i] = self.steps
-            self.paths[i] = self.path
+        self.steps = [self._generate_steps(self.n - 1) for _ in range(n_samples)]
+        self.paths = [self._generate_path(_path_steps) for _path_steps in self.steps]
+
+        self.steps = array(self.steps)
+        self.paths = array(self.paths)
