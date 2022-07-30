641ac0ae4fc353af47b056907af4df581a77559d
Vladimir Sviyazov <sva125125@gmail.com>
2022-07-30 22:18:59 +0300

    [Feature] Implemented a multivariate Geometric Brownian Motion

--- geometric_brownian_motion.py
+++ geometric_brownian_motion.py
@@ -1,69 +1,119 @@
+from __future__ import annotations
+import logging
+
 from brownian_motion import DiscreteWienerProcess
-import numpy as np
+from numpy import array, arange, exp
 
 
 class DiscreteGeometricBrownianMotion(DiscreteWienerProcess):
-    def __init__(self, n=100, initial_position=100, mu=0, sigma=1):
-        DiscreteWienerProcess.__init__(self, n=n, initial_position=0)
+    """
+    Class representing a discrete Geometric Brownian Motion process. If `cov_matrix` is not None,
+    then the process is deemed multivariate. Otherwise `delta_t` must be non None, it will be used
+    as a variance of a univariate process.
+    """
+
+    def __init__(self,
+                 mu: array | list,
+                 sigma: array | list,
+                 initial_position: float | array | list = 100,
+                 delta_t: float = 1,
+                 cov_matrix: array | list = None):
+        """
+
+        Parameters
+        ----------
+        mu: 1D array-like, parameter mu in the process (for each scalar variable in a vector variable)
+        sigma: 1D array-like, parameter sigma in the process (for each scalar variable in a vector variable)
+        initial_position: 1D array-like or float, the starting position of the process, either for each
+        scalar variable (if array-like given) or a scalar number used for all scalar variables
+        delta_t: float, delta t
+        cov_matrix: 2D array-like, covariance matrix of a Wiener process (if multivariate). If not given,
+        the process is deemed to be univariate
+        """
+        self.logger = logging.getLogger(type(self).__name__)
+        self.logger.info(f'Creating an instance of {self.logger.name}')
+
+        super().__init__(initial_position=0,
+                         delta_t=delta_t,
+                         cov_matrix=cov_matrix)
 
         self.initial_position = initial_position
-        self.mu = mu
-        self.sigma = sigma
+        self.mu = array(mu)
+        self.sigma = array(sigma)
+
+        self.delta_t = delta_t
 
         self.w_steps = self.steps
-        self.w_path = self.path
+        self.w_paths = self.paths
 
-        self._drift = self.mu - .5 * self.sigma ** 2
-        self._diffusion = None
+        self.drift = self.mu - .5 * self.sigma ** 2
+        self.diffusion = None
 
-        self._cumulative_drift = self._drift * np.arange(self.n)
-        self._cumulative_diffusion = np.full(shape=(n,), fill_value=np.nan)
+        self.cumulative_drift = None
+        self.cumulative_diffusion = None  # full(shape=(n,), fill_value=nan)
 
-    def generate_path(self):
-        initial_position_temp = self.initial_position  # workaround to keep ini pos for geometric process
-        self.initial_position = 0
-        DiscreteWienerProcess.generate_path(self)
-        self.initial_position = initial_position_temp
+    def _generate_cumulative_drift(self,
+                                   n: int) -> array:
+        """
+        Calculate a cumulative drift of the process of length n
+        Parameters
+        ----------
+        n: int, the # of discrete process states
 
-        self.w_steps = self.steps
-        self.w_path = self.path
+        Returns
+        -------
+        2D numpy.array, the cumulative drift for each variable (each column represents a variable drift)
+        """
+        cumulative_drift = [self.drift] * array([arange(n) for _ in range(self._dimension)]).T * self.delta_t
+        return cumulative_drift
 
-        self._diffusion = self.sigma * self.w_steps
+    def _generate_cumulative_diffusion(self,
+                                       w_path: array) -> array:
+        cumulative_diffusion = [self.sigma] * w_path * self.delta_t
+        return cumulative_diffusion
 
-        self._cumulative_diffusion = self.sigma * self.w_path
+    def _generate_geom_path(self,
+                            cumulative_drift: array,
+                            cumulative_diffusion: array) -> array:
+        path = [self.initial_position] * exp(cumulative_drift + cumulative_diffusion)
+        return path
 
-        self.path = self.initial_position * np.exp(self._cumulative_drift + self._cumulative_diffusion)
+    def generate_paths(self,
+                       n: int,
+                       n_samples: int = 1):
+        """
+        Generate `n_samples` paths of the process, each of length `n`
 
-
-class DiscreteGeometricBrownianMotionSampling(DiscreteGeometricBrownianMotion):
-    def __init__(self, n=100, initial_position=100, mu=0, sigma=1, n_samples=10):
-        DiscreteGeometricBrownianMotion.__init__(self, n=n, initial_position=initial_position, mu=mu, sigma=sigma)
-
+        Parameters
+        ----------
+        n: `int`, number of process states in each sample. Note that it is by 1 larger than #
+        of steps. Thus, for instance, if n = 100, the first state will be according to `initial_position` and 99
+        random states will be generated
+        n_samples: `int`, number of paths
+        """
+        self.n = n
         self.n_samples = n_samples
 
-        self.w_steps_list = np.full(shape=(n_samples, n - 1), fill_value=np.nan)
-        self.w_paths = np.full(shape=(n_samples, n), fill_value=np.nan)
+        initial_position_geom = self.initial_position  # workaround to save ini pos for geometric process
+        self.initial_position = [0 for _ in range(self._dimension)]
+        super().generate_paths(n,
+                               n_samples=n_samples)
+        self.initial_position = initial_position_geom
 
-        self.steps_list = np.full(shape=(n_samples, n - 1), fill_value=np.nan)
-        self.paths = np.full(shape=(n_samples, n), fill_value=np.nan)
+        self.w_steps = self.steps.copy()
+        self.w_paths = self.paths.copy()
+        self.steps = None
 
-        self._drifts = np.full(shape=(n_samples, n), fill_value=self._drift)
-        self._diffusions = np.full(shape=(n_samples, n - 1), fill_value=np.nan)
+        self.cumulative_drift = [self._generate_cumulative_drift(n) for _ in range(self.n_samples)]
 
-        self._cumulative_drifts = np.full(shape=(n_samples, n), fill_value=self._cumulative_drift)
-        self._cumulative_diffusions = np.full(shape=(n_samples, n), fill_value=np.nan)
+        self.diffusion = [self._generate_cumulative_diffusion(_w_steps) for _w_steps in self.w_steps]
+        self.cumulative_diffusion = [self._generate_cumulative_diffusion(_w_steps) for _w_steps in self.w_paths]
 
-    def generate_paths(self):
-        for i in range(self.n_samples):
-            self.generate_path()
+        self.paths = [self._generate_geom_path(_cumulative_drift, _cumulative_diffusion)
+                      for (_cumulative_drift, _cumulative_diffusion)
+                      in zip(self.cumulative_drift, self.cumulative_diffusion)]
 
-            self.steps_list[i] = self.steps
-            self.paths[i] = self.path
-
-            self.w_steps_list = self.w_steps
-            self.w_paths = self.w_path
-
-            self._diffusions[i] = self._diffusion
-            self._drifts[i] = self._drift
-
-            self._cumulative_diffusions[i] = self._cumulative_diffusion
+        self.cumulative_drift = array(self.cumulative_drift).copy()
+        self.diffusion = array(self.diffusion).copy()
+        self.cumulative_diffusion = array(self.cumulative_diffusion).copy()
+        self.paths = array(self.paths).copy()
